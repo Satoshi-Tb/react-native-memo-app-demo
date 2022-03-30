15084551c73b945ca899c106245b85a2e7d7882e
Satoshi T <sulp91_fy@yahoo.co.jp>
2022-03-31 01:35:46 +0900

    Save memo on firestore

--- src/screens/MemoCreateScreen.js
+++ src/screens/MemoCreateScreen.js
@@ -1,25 +1,52 @@
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
 import {
   StyleSheet,
   View,
   TextInput,
   KeyboardAvoidingView,
+  Alert,
 } from "react-native";
 import { CircleButton } from "../components/CircleButton";
+import { auth, db } from "../lib/firebase";
 
 export const MemoCreateScreen = (props) => {
   const { navigation } = props;
+  const [memo, setMemo] = useState("");
+
+  const handlePress = async () => {
+    if (memo === "") {
+      Alert.alert("Warning", "メモ本文を入力してください。");
+      return;
+    }
+    try {
+      const { currentUser } = auth;
+      const ref = await collection(db, `users/${currentUser.uid}/memos`);
+      const docRef = await addDoc(ref, {
+        bodyText: memo,
+        updatedAt: new Date(),
+      });
+      console.log("Created: " + docRef.id);
+      navigation.goBack();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "メモ登録に失敗しました");
+    }
+  };
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="height">
       <View style={styles.inputContainer}>
-        <TextInput value="" multiline={true} style={styles.input} />
+        <TextInput
+          value={memo}
+          multiline={true}
+          style={styles.input}
+          onChangeText={(text) => setMemo(text)}
+          autoFocus={true}
+          keyboardType="ascii-capable"
+        />
       </View>
-      <CircleButton
-        name="check"
-        color="white"
-        onPress={() => {
-          navigation.goBack();
-        }}
-      />
+      <CircleButton name="check" color="white" onPress={handlePress} />
     </KeyboardAvoidingView>
   );
 };
